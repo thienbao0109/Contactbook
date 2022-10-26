75d645d70caebb802e5e520cf69b31961a48c993
thienbao0109 <baob1809678@student.ctu.edu.vn>
2022-10-26 10:10:28 +0700

    update code

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+  import AppHeader from '@/components/AppHeader.vue';
+  export default {
+    components: {
+    AppHeader,
+    },
+  }
+</script>
+
+<template>
+  <AppHeader></AppHeader>
+  <div class="container mt-3">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style>
+  .page{
+    max-width: 400px;
+    margin: auto;
+  }
+</style>
+
